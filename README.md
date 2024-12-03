659568dc407eb9a2c9ca3510111d7e9301bd3ced
Chlorus <99441357+Chlorus3@users.noreply.github.com>
2024-12-03 20:22:33 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Manim Animated Heart
+
+![alt text](assets/HeartGraph.png)
+
+## Requirements
+
+See how to download Manim [here](https://www.manim.community/) and some other requirements that may be needed
+
+## How to run
+
+```txt
+manim main.py DrawHeart
+```
---
+++ main.py
@@ -0,0 +1,44 @@
+from manim import *
+import numpy as np
+
+class DrawHeart(Scene):
+	def construct(self):
+		# Setup axes
+		axes = Axes(
+			x_range = [-np.sqrt(3), np.sqrt(3)],
+			y_range = [-2, 2],
+			axis_config = {"include_numbers": False},
+		)
+		
+		# Setup k value, this will increase to draw the heart
+		k_value = ValueTracker(0)
+		K_VALUE_MAX = 100
+
+		# Tell manim to always redraw the function as the k_value increases
+		heart = always_redraw(
+			lambda: FunctionGraph(
+				lambda x: self.heart_function(x, k_value.get_value()),
+				x_range = [-np.sqrt(3), np.sqrt(3)], # DO NOT CHANGE
+				color = RED
+			)
+		)
+
+		# Draw the axes and the initial graph when k = 0
+		self.play(Write(axes))
+		self.play(Write(heart))
+
+		# For 10 seconds, increase k to 100
+		self.play(
+			k_value.animate.set_value(100),
+			run_time = 10,
+			rate_func = smooth	
+		)
+
+
+	def heart_function(self, x, k):
+		"""
+		Important note:
+		- The function only returns a real number when |x| <= sqrt(3) and the input range
+		    is assumed to be between the range, if outside, nan will be returned
+		"""
+		return np.cbrt(np.square(x)) + 0.9 * np.sin(k * x) * np.sqrt(3 - np.square(x))
